7177ce480b1833280b21cb5a515e11a11704aab4
theblackaliens <koodokage@gmail.com>
2023-12-05 15:46:10 +0300

    Project created

---
+++ Source/ActionRoqueLike/ActionRoqueLike.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ActionRoqueLike.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ActionRoqueLike, "ActionRoqueLike" );
---
+++ Source/ActionRoqueLike/ActionRoqueLikeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ActionRoqueLikeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONROQUELIKE_API AActionRoqueLikeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
